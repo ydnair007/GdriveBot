ff9f3e6140e5f35a41b92a6a8913af43f986fc91
ydnair007 <33169773+ydnair007@users.noreply.github.com>
2020-08-16 01:42:45 +0530

    Update test.py
    Mega Credentials

--- mega/test.py
+++ mega/test.py
@@ -1,5 +1,5 @@
 import mega
 
-m = Mega.from_credentials("followaryan8@gmail.com","Vikash@12")
+m = Mega.from_credentials("ydnair007@gmail.com","itkimakichut")
                     filename=m.download_from_url(url)
                     print("Downloading Complete Mega :", filename)
